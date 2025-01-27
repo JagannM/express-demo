bf28f649425329d8ed141dffb6a19c578a27d44b
JagannM <jthedarkknight@gmail.com>
2025-01-27 08:37:51 +0530

    express server using get

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const app = express();
-app.set('title','Hello');
+
+/* app.set('title','Hello');
 var title = app.get('title');
-console.log(title);
+console.log(title); */
+
+app.get('/',(req,res)=>{
+    res.send('Hello World!!!');
+});
+
+app.get('/api/courses',(req,res)=>{
+    res.send([1,2,3]);
+});
+app.listen(3000,()=>{console.log('Listening on port 3000')});
