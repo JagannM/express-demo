71745e8589148a9119dcf74454887478584ea56f
JagannM <jthedarkknight@gmail.com>
2025-01-27 21:00:01 +0530

    POST demo with help of postman app

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json()); 
+
 const courses = [
     {id:1, name:'course1'},
     {id:2, name:'course2'},
@@ -23,11 +25,24 @@ app.get('/api/courses/:id',(req,res)=>{
     res.send(req.params.id);
 }) */
 
-    app.get('/api/courses/:id',(req,res)=>{
+    /*  app.get('/api/courses/:id',(req,res)=>{
         const course = courses.find(c => c.id === parseInt(req.params.id));
-        if(!course) res.status(404).send("The course is not found");
+        if(!course) res.status(404).send("The course with the given ID is not found");
         res.send(course);
-    })
+    }) */
+
+app.post('/api/courses',(req,res)=>{
+const course = {
+    id: courses.length + 1,
+    name: req.body.name
+};
+
+courses.push(course);
+res.send(course);
+});
+
+
+
 
 //app.listen(3000,()=>{console.log('Listening on port 3000')});
 
