4beced2a916e47de448f22e34f58a73a052ff11f
JagannM <jthedarkknight@gmail.com>
2025-01-29 10:15:00 +0530

    Handling HTTP PUT requests with joi npm package, without validation logic in a separate function

--- index.js
+++ index.js
@@ -19,25 +19,25 @@ console.log(title); */
 
 app.get('/api/courses',(req,res)=>{
     res.send(['a','b','c']);
-});
+}); 
 
 app.get('/api/courses/:id',(req,res)=>{
     res.send(req.params.id);
 }) */
 
-    /*  app.get('/api/courses/:id',(req,res)=>{
+     app.get('/api/courses/:id',(req,res)=>{
         const course = courses.find(c => c.id === parseInt(req.params.id));
         if(!course) res.status(404).send("The course with the given ID is not found");
         res.send(course);
-    }) */
+    })
 
-app.post('/api/courses',(req,res)=>{
+/* app.post('/api/courses',(req,res)=>{
 
-/* //basic input validation
-if(!req.body.name || req.body.name.length < 3){
-    res.status(400).send("Name is required and it should be minimum 3 characters"); // 400 Bad Request
-    return;
-} */
+//basic input validation
+//if(!req.body.name || req.body.name.length < 3){
+//    res.status(400).send("Name is required and it should be minimum 3 characters"); // 400 Bad Request
+//    return;
+// }
 
 //joi schema for the data object
 const schema = Joi.object({
@@ -46,8 +46,9 @@ const schema = Joi.object({
 const result = schema.validate(req.body);
 //console.log(result);
 if (result.error){
-    res.status(400)
-.send(result.error.details[0].message)};
+    res.status(400).send(result.error.details[0].message);
+    return;
+};
 
 const course = {
     id: courses.length + 1,
@@ -56,11 +57,36 @@ const course = {
 
 courses.push(course);
 res.send(course);
-});
+}); */
 
 /* app.get('/api/courses',(req,res)=>{
     res.send(courses);
 }); */
+
+
+app.put('/api/courses/:id',(req,res) => {
+    //Lookup the course and if not existing, return 404
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) res.status(404).send("The course with the given ID is not found");
+
+    //validate the course, if invalid, return 400
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    });
+    const result = schema.validate(req.body);
+    if (result.error){
+        res.status(400).send(result.error.details[0].message);
+        return;
+    };
+
+    //update the course
+    course.name = req.body.name;
+
+    //return the updated course
+    res.send(course);
+})
+
+
 
 
 
